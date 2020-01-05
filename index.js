d3a79ee9ca159ccdc3c6a91d5efef4616d198325
averagealloy <m.mctighe10@gmail.com>
2020-01-05 17:06:27 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution here
+function findMatching(a, b) {
+    return a.filter(possibleMatch => 
+        possibleMatch.toLowerCase() === b.toLowerCase()
+     )
+}
+
+function fuzzyMatch(a, string){
+    return a.filter(possibleMatch =>
+        possibleMatch.toLowerCase().indexOf(string.toLowerCase())=== 0
+        )
+}
+
+function matchName(a, name) {
+    return a.filter(record => record.name === name)
+}
