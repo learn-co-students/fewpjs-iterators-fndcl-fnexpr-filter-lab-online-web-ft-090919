9f03b1f03c1dca1edfc0b8c09458bf58a996f0b0
Matt Plichta <bigcatplichta@gmail.com>
2020-01-20 23:22:23 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solution here
+function findMatching(list, searchTerm) {
+  let result = list.filter(match =>
+  // convert searchTerm and match to lower case
+  match.toLowerCase() === searchTerm.toLowerCase()
+  )
+  return result
+}
+
+function fuzzyMatch(list, searchTerm) {
+  return list.filter(match => 
+  // convert match and searchTerm to lowercase 
+  // still trying to wrap my head around .indexOf
+  match.toLowerCase().indexOf(searchTerm.toLowerCase()) === 0)
+}
+
+function matchName(list, searchTerm) {
+  return list.filter(match => match.name === searchTerm)
+}
